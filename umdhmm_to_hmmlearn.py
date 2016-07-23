5343f62f554dbb1f5d2a565196b988c672c9919c
Miha Filej <miha@filej.net>
2016-07-23 12:41:33 +0200

    Accept separate arguments for hmm and key files

--- umdhmm_to_hmmlearn.py
+++ umdhmm_to_hmmlearn.py
@@ -17,15 +17,17 @@ args = argparse.ArgumentParser(
         Convert UMDHMM .hmm and .key files to pickle dumps of
         hmmlearn.MultinomialHMM and LabelEncoder objects.
         """)
-args.add_argument("source",
-        help="path to source UMDHMM files (expects SOURCE.hmm and SOURCE.key files)")
-args.add_argument("target",
-        help="path where hmmlearn model will be written (TARGET.hmm and TARGET.le)")
+args.add_argument("hmm",
+        help="path to source UMDHMM model file)")
+args.add_argument("key",
+        help="path to source .key file")
+args.add_argument("out",
+        help="basename for output files")
 args = args.parse_args()
 
-with open('{0}.hmm'.format(args.source)) as f:
+with open(args.hmm) as f:
     umd = UmdhmmFile(f)
-with open('{0}.key'.format(args.source)) as f:
+with open(args.key) as f:
     kf = KeyFile(f)
 
 mhmm = MultinomialHMM(n_components=umd.n, init_params='')
@@ -36,8 +38,11 @@ mhmm.emissionprob_ = umd.emissionprob_
 le = LabelEncoder()
 le.classes_ = np.array(kf.classes)
 
-joblib.dump(mhmm, '{0}.pkl'.format(args.target))
-with open('{0}.le'.format(args.target), "wb") as f:
+out_pkl = '{0}.pkl'.format(args.out)
+out_le = '{0}.le'.format(args.out)
+
+joblib.dump(mhmm, out_pkl)
+with open(out_le, "wb") as f:
     pickle.dump(le, f)
 
-print("Output written to:\n\t- {0}.pkl\n\t- {0}.le".format(args.target), file=sys.stderr)
+print("Output written to:\n\t- {0}\n\t- {1}".format(out_pkl, out_le), file=sys.stderr)
